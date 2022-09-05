c8979d44073565991952945511fbce9f203fad3a
Gabriel Catani <contatogcatani@gmail.com>
2022-09-05 20:59:39 -0300

    test heroku deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { CowModule } from './cow/cow.module';
 import { ApiModule } from './api/api.module';
@@ -8,7 +7,7 @@ import { VaccineModule } from './vaccine/vaccine.module';
 
 @Module({
   imports: [UserModule, CowModule, ApiModule, VaccineModule],
-  controllers: [AppController],
+  controllers: [],
   providers: []
 })
 export class AppModule {}
